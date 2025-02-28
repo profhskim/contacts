7fd02dbea54d99b24abbd51ba0bd3280ca06d3c6
profhskim <profhskim2@gmail.com>
2025-02-28 12:45:30 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dbConnect = require('./config/dbConnect');
+const app = express();
+const port = 3000;
+
+/* const logger = (req, res, next) => {
+  console.log('User Logged');
+  next();
+}; */
+
+dbConnect();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/contacts', require('./routes/contactRoutes'));
+
+app.listen(port, () => {
+  console.log(`${port}번 포트에서 서버 실행 중`);
+});
